968fba23bd0795b317e9e65581e49a12268c2d60
RJ <rongjihuang@gmail.com>
2019-02-28 17:45:07 +0800

    Delete unnecessary files

--- src/main/java/tech/simter/start/r2dbc/spring/standardcurd/PeopleRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.simter.start.r2dbc.spring.standardcurd;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-/**
- * @author RJ
- */
-public interface PeopleRepository extends ReactiveCrudRepository<People, String> {
-}
--- src/main/resources/sql/postgres/schema.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists dream;
-create table dream (
-  id        int primary key,
-  name      varchar(50) not null,
-  create_on timestamp
-);
