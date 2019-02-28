7c427faa12001efb42cdfc72ca0ef7d76f8a14a9
RJ <rongjihuang@gmail.com>
2019-02-28 17:34:38 +0800

    Add insert, select test to ConnectionFactoryTest

--- src/test/java/tech/simter/start/r2dbc/ConnectionFactoryTest.java
+++ src/test/java/tech/simter/start/r2dbc/ConnectionFactoryTest.java
@@ -27,34 +27,56 @@ class ConnectionFactoryTest {
   @Autowired
   private ConnectionFactory connectionFactory;
 
-  private Mono<Connection> connection() {
-    return Mono.from(connectionFactory.create());
+  private boolean cache = true;
+
+  // build connection
+  private Mono<Connection> connection(boolean cache) {
+    if (cache) {                 // cache
+      if (cacheConnection == null) cacheConnection = Mono.from(connectionFactory.create());
+      return cacheConnection;
+    } else {                     // new
+      logger.warn("create a new connection");
+      return Mono.from(connectionFactory.create());
+    }
+  }
+
+  // a cache connection
+  private Mono<Connection> cacheConnection;
+
+  private int id = 100;
+
+  private int nextId() {
+    return ++id;
   }
 
   @BeforeAll
-  void createTable() {
+  void setup() {
+    logger.warn("cache={}, connection will {}be reused for all test method.", cache, cache ? "" : "not ");
+    // create test table
     String sql = "create table T(id int primary key);";
     StepVerifier.create(
-      connection()
+      connection(cache)
         .flatMapMany(c -> c.createStatement(sql).execute())
-        .flatMap(Result::getRowsUpdated)
-    ).expectNext(0).verifyComplete();
+        //.flatMap(Result::getRowsUpdated) // TODO : h2 return 0, postgres return empty
+        .then()
+    ).verifyComplete();
   }
 
   @AfterAll
-  void dropTable() {
-    // success
+  void release() {
+    // drop test table
     String sql = "drop table if exists T;";
     StepVerifier.create(
-      connection()
+      connection(cache)
         .flatMapMany(c -> c.createStatement(sql).execute())
-        .flatMap(Result::getRowsUpdated)
-    ).expectNext(0).verifyComplete();
+        //.flatMap(Result::getRowsUpdated) // TODO : h2 return 0, postgres return empty
+        .then()
+    ).verifyComplete();
 
-    // error because table not exists
+    // test drop error
     String sql1 = "drop table T;";
     StepVerifier.create(
-      connection()
+      connection(cache)
         .flatMapMany(c -> c.createStatement(sql1).execute())
         .flatMap(Result::getRowsUpdated)
     ).consumeErrorWith(e -> {
@@ -68,57 +90,87 @@ class ConnectionFactoryTest {
         assertEquals("?", e.getMessage());
       }
     }).verify();
+
+    // close connection
+    if (cacheConnection != null)
+      StepVerifier.create(cacheConnection.flatMapMany(Connection::close))
+        .verifyComplete();
   }
 
   @Test
   void deleteAll() {
-    String sql = "delete from T";
-
-    // success
     StepVerifier.create(
-      connection()
-        .flatMapMany(c -> c.createStatement(sql).execute())
+      connection(cache)
+        .flatMapMany(c -> c.createStatement("delete from T").execute())
         .then()
     ).verifyComplete();
   }
 
+  /**
+   * with r2dbc-1.0.0.M6 :
+   * 1. postgres : success
+   * 2. h2: failed (expected: onNext(1); actual: onError(org.h2.message.DbException: Method is only allowed for a query. Use execute or executeUpdate instead of executeQuery [90002-197]))
+   */
   @Test
-  @Disabled("test failed in h2: org.h2.message.DbException: Method is only allowed for a query. Use execute or executeUpdate instead of executeQuery [90002-197])")
-  void insertNothing() {
-    String sql = "insert into T(id) select id from T where id is null";
-
-    // success and get effected row count
+  void insertBySelect() {
+    // insert test data
+    int id1 = nextId();
     StepVerifier.create(
-      connection()
-        .flatMapMany(c -> c.createStatement(sql).execute())
-        .flatMap(Result::getRowsUpdated)
-    ).expectNext(0).verifyComplete();
-  }
-
-  @Test
-  void insertOne() {
-    String sql = "insert into T(id) values($1)";
-
-    // success and get effected row count
-    StepVerifier.create(
-      connection()
+      connection(cache)
         .flatMapMany(c ->
-          c.createStatement(sql)
-            .bind("$1", 1)
+          c.createStatement("insert into T(id) values($1)")
+            .bind("$1", id1)
             .execute()
         )
         .flatMap(Result::getRowsUpdated)
     ).expectNext(1).verifyComplete();
 
-    // success and get result but no result
+    // insert by select
+    int id2 = nextId();
+    String sql = "insert into T(id) select $2 from T where id = $1";
     StepVerifier.create(
-      connection()
+      connection(cache)
+        .flatMapMany(c -> c.createStatement(sql)
+          .bind("$1", id1)
+          .bind("$2", id2)
+          .execute())
+        .flatMap(Result::getRowsUpdated)
+    ).expectNext(1).verifyComplete();
+  }
+
+  @Test
+  void insertOneAndGetUpdatedCount() {
+    int id = nextId();
+    String sql = "insert into T(id) values($1)";
+
+    StepVerifier.create(
+      connection(cache)
         .flatMapMany(c ->
           c.createStatement(sql)
-            .bind("$1", 2)
+            .bind("$1", id)
+            .execute()
+        ).flatMap(Result::getRowsUpdated)
+    ).expectNext(1).verifyComplete();
+  }
+
+  /**
+   * with r2dbc-1.0.0.M6 :
+   * 1. postgres : run forever
+   * 2. h2: not invoked
+   */
+  @Test
+  @Disabled("postgres will run forever")
+  void insertOneAndDoResultMap() {
+    int id = nextId();
+    String sql = "insert into T(id) values($1)";
+    StepVerifier.create(
+      connection(cache)
+        .flatMapMany(c ->
+          c.createStatement(sql)
+            .bind("$1", id)
             .execute()
         )
-        .flatMap(result -> result.map((row, rowMetadata) -> { // not invoked because no rows
+        .flatMap(result -> result.map((row, rowMetadata) -> { // h2: not invoked because no rows
           logger.debug("result columns :");
           rowMetadata.getColumnMetadatas().forEach(c ->
             logger.debug("  name={}, type={}, precision={}", c.getName(), c.getType(), c.getPrecision().orElse(-1))
@@ -126,45 +178,149 @@ class ConnectionFactoryTest {
           return 1;
         }))
     ).verifyComplete();
+  }
 
-    // error because primary key unique constrain
+  @Test
+  void insertFailedByUniqueConstrain() {
+    // insert test data
+    int id = nextId();
+    String sql = "insert into T(id) values($1)";
     StepVerifier.create(
-      connection()
+      connection(cache)
         .flatMapMany(c ->
           c.createStatement(sql)
-            .bind("$1", 1)
+            .bind("$1", id)
+            .execute()
+        )
+        .flatMap(Result::getRowsUpdated)
+    ).expectNext(1).verifyComplete();
+
+    // insert it again
+    StepVerifier.create(
+      connection(cache)
+        .flatMapMany(c ->
+          c.createStatement(sql)
+            .bind("$1", id)
             .execute()
         )
         .flatMap(Result::getRowsUpdated)
     ).consumeErrorWith(e -> {
       // logger.error("insertOne error", e);
       String clazz = e.getClass().getName();
-      if (clazz.startsWith("org.h2")) {
+      if (clazz.contains(".h2.")) {
         assertEquals("org.h2.message.DbException", clazz);
         assertEquals("Unique index or primary key violation: \"PRIMARY KEY ON PUBLIC.T(ID)\" [23505-197]", e.getMessage());
-      } else if (clazz.startsWith("org.postgresql")) {
-        assertEquals("org.postgresql.?", clazz);
-        assertEquals("?", e.getMessage());
+      } else if (clazz.contains(".postgresql.")) {
+        assertEquals("io.r2dbc.postgresql.PostgresqlServerErrorException", clazz);
+        assertEquals("重复键违反唯一约束\"t_pkey\"", e.getMessage());
+      } else {
+        throw new RuntimeException("TODO : other database");
       }
     }).verify();
   }
 
   @Test
-  void insertTwo() {
-    // multiple insert into
-    String sql = "insert into T(id) values(10);insert into T(id) values(11), (12);";
+  void insertRowsBySingleInsertInto() {
+    int[] ids = new int[]{nextId(), nextId()};
+    String sql = "insert into T(id) values($1), ($2)";
     StepVerifier.create(
-      connection()
+      connection(cache)
+        .flatMapMany(c -> c.createStatement(sql)
+          .bind("$1", ids[0])
+          .bind("$2", ids[1])
+          .execute())
+        .flatMap(Result::getRowsUpdated)
+    ).expectNext(2).verifyComplete();
+  }
+
+  @Test
+  void insertRowsByMultipleInsertIntoWithoutBind() {
+    int[] ids = new int[]{nextId(), nextId(), nextId()};
+    String sql = "insert into T(id) values(" + ids[0] + ");\ninsert into T(id) values(" + ids[1] + "), (" + ids[2] + ");";
+    StepVerifier.create(
+      connection(cache)
         .flatMapMany(c -> c.createStatement(sql).execute())
         .flatMap(Result::getRowsUpdated) // each 'insert into' return one result
     ).expectNext(1).expectNext(2).verifyComplete();
+  }
 
-    // single insert into
-    String sql1 = "insert into T(id) values(15), (16);";
+  /**
+   * with r2dbc-1.0.0.M6 :
+   * 1. postgres : failed (expected: onNext(1); actual: onError(java.lang.UnsupportedOperationException: Binding parameters is not supported for the statement 'insert into T(id) values($1);
+   * insert into T(id) values($2), ($3);'))
+   * 2. h2: failed (expected: onNext(1); actual: onError(java.lang.ArrayIndexOutOfBoundsException: 3))
+   */
+  @Test
+  void insertRowsByMultipleInsertIntoWithBind() {
+    int[] ids = new int[]{nextId(), nextId(), nextId()};
+    String sql = "insert into T(id) values($1);\ninsert into T(id) values($2), ($3);";
     StepVerifier.create(
-      connection()
-        .flatMapMany(c -> c.createStatement(sql1).execute())
+      connection(cache)
+        .flatMapMany(c -> c.createStatement(sql)
+          .bind("$1", ids[0])
+          .bind("$2", ids[1])
+          .bind("$3", ids[3])
+          .execute())
+        .flatMap(Result::getRowsUpdated)
+    ).expectNext(1).expectNext(2).verifyComplete();
+  }
+
+  @Test
+  void selectBySingleLineSql() {
+    // insert test data
+    int[] ids = new int[]{nextId(), nextId()};
+    StepVerifier.create(
+      connection(cache)
+        .flatMapMany(c -> c.createStatement("insert into T(id) values($1), ($2)")
+          .bind("$1", ids[0])
+          .bind("$2", ids[1])
+          .execute())
         .flatMap(Result::getRowsUpdated)
     ).expectNext(2).verifyComplete();
+
+    // select them
+    String sql = "select id from T where id in ($1, $2) order by id asc";
+    StepVerifier.create(
+      connection(cache)
+        .flatMapMany(c -> c.createStatement(sql)
+          .bind("$1", ids[0])
+          .bind("$2", ids[1])
+          .execute())
+        .flatMap(result -> result.map((row, metadata) -> row.get("id", Integer.class)))
+    ).expectNext(ids[0]).expectNext(ids[1]).verifyComplete();
+  }
+
+  /**
+   * with r2dbc-1.0.0.M6 :
+   * 1. postgres : failed (expected: onNext(101); actual: onError(java.lang.UnsupportedOperationException: Binding parameters is not supported for the statement 'select id from T
+   * where id in ($1, $2)
+   * order by id asc'))
+   * 2. h2: expected: onNext(101); actual: onError(org.h2.message.DbException: Method is not allowed for a query. Use execute or executeQuery instead of executeUpdate [90001-197]))
+   */
+  @Test
+  void selectByMultiLineSql() {
+    // insert test data
+    int[] ids = new int[]{nextId(), nextId()};
+    StepVerifier.create(
+      connection(cache)
+        .flatMapMany(c -> c.createStatement("insert into T(id) values($1), ($2)")
+          .bind("$1", ids[0])
+          .bind("$2", ids[1])
+          .execute())
+        .flatMap(Result::getRowsUpdated)
+    ).expectNext(2).verifyComplete();
+
+    // select them
+    String sql = "select id from T" +
+      "\n  where id in ($1, $2)" +
+      "\n  order by id asc";
+    StepVerifier.create(
+      connection(cache)
+        .flatMapMany(c -> c.createStatement(sql)
+          .bind("$1", ids[0])
+          .bind("$2", ids[1])
+          .execute())
+        .flatMap(result -> result.map((row, metadata) -> row.get("id", Integer.class)))
+    ).expectNext(ids[0]).expectNext(ids[1]).verifyComplete();
   }
 }
