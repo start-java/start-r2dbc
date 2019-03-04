26268cbd3de7060f6204f64fe5af4d9e36bc7f40
RJ <rongjihuang@gmail.com>
2019-03-04 23:55:19 +0800

    Add SPI bind java.util.Date|Calendar param test

---
+++ src/test/java/tech/simter/start/r2dbc/spi/bindparam/BindUtilCalendarToSqlTimestampTest.java
@@ -0,0 +1,96 @@
+package tech.simter.start.r2dbc.spi.bindparam;
+
+import io.r2dbc.spi.Result;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+import tech.simter.start.r2dbc.spi.AbstractSpiTest;
+
+import java.util.Calendar;
+import java.util.Optional;
+
+import static tech.simter.start.r2dbc.TestUtils.nextId;
+
+/**
+ * @author RJ
+ */
+class BindUtilCalendarToSqlTimestampTest extends AbstractSpiTest {
+  @Override
+  protected String getCreateTableSql() {
+    return "create table t(id int primary key, value timestamp)";
+  }
+
+  @Test
+  void notNullValue() {
+    // insert into
+    Integer id = nextId();
+    Calendar value = Calendar.getInstance();
+    connection()
+      .flatMapMany(c -> c.createStatement("insert into t(id, value) values($1, $2)")
+        .bind("$1", id)
+        .bind("$2", value)
+        .execute())
+      .flatMap(Result::getRowsUpdated)
+      .as(StepVerifier::create)
+      .expectNext(1)
+      .verifyComplete();
+
+    // select
+    connection()
+      .flatMapMany(c -> c.createStatement("select value from t where id = $1")
+        .bind("$1", id)
+        .execute())
+      .flatMap(result -> result.map((row, metadata) -> row.get("value", Calendar.class)))
+      .as(StepVerifier::create)
+      .expectNext(value)
+      .verifyComplete();
+
+    // update
+    Calendar newValue = Calendar.getInstance();
+    newValue.add(Calendar.MINUTE, -1);
+    connection()
+      .flatMapMany(c -> c.createStatement("update t set value = $2 where id = $1")
+        .bind("$1", id)
+        .bind("$2", newValue)
+        .execute())
+      .flatMap(Result::getRowsUpdated)
+      .as(StepVerifier::create)
+      .expectNext(1)
+      .verifyComplete();
+  }
+
+  @Test
+  void nullValue() {
+    // insert into
+    Integer id = nextId();
+    connection()
+      .flatMapMany(c -> c.createStatement("insert into t(id, value) values($1, $2)")
+        .bind("$1", id)
+        .bindNull("$2", Calendar.class)
+        .execute())
+      .flatMap(Result::getRowsUpdated)
+      .as(StepVerifier::create)
+      .expectNext(1)
+      .verifyComplete();
+
+    // select
+    connection()
+      .flatMapMany(c -> c.createStatement("select value from t where id = $1")
+        .bind("$1", id)
+        .execute())
+      .flatMap(result -> result.map((row, metadata) -> Optional.ofNullable(row.get("value", Calendar.class))))
+      .as(StepVerifier::create)
+      .expectNext(Optional.ofNullable(null))
+      .verifyComplete();
+
+    // update
+    connection()
+      .flatMapMany(c -> c.createStatement("update t set value = $2 where id = $1")
+        .bind("$1", id)
+        .bindNull("$2", Calendar.class)
+        .execute())
+      .flatMap(Result::getRowsUpdated)
+      .as(StepVerifier::create)
+      .expectNext(1)
+      .verifyComplete();
+  }
+}
